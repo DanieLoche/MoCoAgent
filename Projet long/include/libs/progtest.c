ab3c58c8b4518022e6055f33d2f1e974d7e3d887
BrixPyxis <danieloche@gmail.com>
2019-10-16 16:22:13 +0200

    MaJ Librairies et Programme de Test

--- Projet long/include/libs/progtest.c
+++ Projet long/include/libs/progtest.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <iostream>
-
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <vector>
 #include <chrono>
 using namespace std::chrono;
+using namespace std;
 
 extern "C" {
 	int basicmath_small   (int argc, char *argv[]);
@@ -29,30 +33,140 @@ extern "C" {
 	int gsm_func          (int argc, char *argv[]);
 }
 
+std::string trim(const std::string& str,
+                 const std::string& whitespace = " ")
+{
+    const auto strBegin = str.find_first_not_of(whitespace);
+    if (strBegin == std::string::npos)
+        return ""; // no content
+
+    const auto strEnd = str.find_last_not_of(whitespace);
+    const auto strRange = strEnd - strBegin + 1;
+
+    return str.substr(strBegin, strRange);
+}
+
+std::string reduce(const std::string& str,
+                   const std::string& fill = " ",
+                   const std::string& whitespace = " \t")
+{
+    // trim first
+    auto result = trim(str, whitespace);
+
+    // replace sub ranges
+    auto beginSpace = result.find_first_of(whitespace);
+    while (beginSpace != std::string::npos)
+    {
+        const auto endSpace = result.find_first_not_of(whitespace, beginSpace);
+        const auto range = endSpace - beginSpace;
+
+        result.replace(beginSpace, range, fill);
+
+        const auto newStart = beginSpace + fill.length();
+        beginSpace = result.find_first_of(whitespace, newStart);
+    }
+
+    return result;
+}
+
+int _argc = 0;
+std::vector<char*> _argv;
+
+void manageArguments(char* argv[], string arguments)
+{
+
+	arguments = reduce(arguments);
+	int chr;
+	for (chr = 0; arguments[chr] != '\0'; chr++)
+	{
+	  if (arguments[chr] == '\n' || arguments[chr] == '\r')
+		 arguments[chr] = '\0';
+	}
+
+	std::istringstream iss( arguments);
+	cout << "\nManaging arguments : [" << arguments << "]" << endl;
+	string token;
+	int nextStr = 0;
+	//_argc = 1;
+	while (getline(iss, token, ' '))
+	{
+		token = reduce(token);
+		cout << "Managing token [" << token << "]." << endl;
+		if (token == "<")
+			nextStr = 1;
+		else if (token == ">")
+			nextStr = 2;
+		else if (token != "")
+		{
+			if (nextStr == 1)
+			{
+				cout << "Tried to change StdIn to : " << token << endl;
+			}
+			else if (nextStr == 2)
+			{
+				cout << "Tried to change StdOut to : " << token << endl;
+			}
+			else
+			{
+				char *arg = new char[token.size() +1];  // +1
+				copy(token.begin(), token.end(), arg);
+				arg[token.size()]= '\0';
+				cout << "token : [" << token << "] (" << token.size() << ") copied to [" << arg << "] (" << strlen(arg) << ")." << endl;
+
+				_argv.push_back(arg);
+				_argc++;
+			}
+			nextStr = 0;
+		}
+	}
+	_argv.push_back(0);
+}
+
 int main(int argc, char *argv[])
 {
 	printf("Debut test\n");
+
+	  int i = 0;
+	  _argv.push_back(argv[0]);
+	  _argc++;
+	  for (i=1; i < argc; i++)
+	  {
+		  std::string toPrint;
+		  if (argv[i]==NULL)toPrint = "null";
+		  else toPrint = argv[i];
+		  std::cerr << "Arg #" << i << " = " << toPrint << " ; ";
+		  manageArguments(argv, argv[i]);
+	  }
+	  std::cerr << " with (" << _argc << ") elements." << std::endl;
+	  cerr << "Bilan paramètres donnés : \n";
+	  for (i=0; i < (int)_argv.size()-1; i++)
+	  {
+		  std::cerr << "Arg #" << i << " = " << _argv[i] << " ; ";
+	  }
+	  cerr << endl;
+	  cerr << "Total : " << _argv.size()-1 << " arguments." << endl;
+
 	int ret = 0;
 	auto start = high_resolution_clock::now();
-	ret += basicmath_small( argc, argv);
-	auto mid = high_resolution_clock::now();
-	ret += basicmath_large( argc, argv);
+// ret += basicmath_small( argc, argv);
+// ret += basicmath_large( argc, argv);
 //	ret += bitcount_func( argc, argv);
 //	ret += qsort_small( argc, argv) ;
 //	ret += qsort_large( argc, argv) ;
 //	ret += susan( argc, argv);
 //	ret += djpeg_func( argc, argv);
-//	ret += cjpeg_func( argc, argv);
+	ret += cjpeg_func( _argv.size()-1, &_argv[0]);
 //	ret += dijkstra_small( argc, argv) ;
 //	ret += dijkstra_large( argc, argv) ;
 //	ret += patricia( argc, argv);
 //	ret += stringsearch_small( argc, argv);
 //	ret += stringsearch_large( argc, argv);
-//	ret += blowfish( argc, argv);
+//	ret += blowfish( _argv.size()-1, &_argv[0]);
 //	ret += rijndael( argc, argv);
 //	ret += sha( argc, argv);
-//	ret += rawdaudio( argc, argv);
-//	ret += rawcaudio( argc, argv);
+// ret += rawdaudio( argc, argv);
+	auto mid = high_resolution_clock::now();
+// ret += rawcaudio( argc, argv);
 //	ret += crc( argc, argv);
 //	ret += fft( argc, argv) ;
 //	ret += typeset_func( argc, argv);
