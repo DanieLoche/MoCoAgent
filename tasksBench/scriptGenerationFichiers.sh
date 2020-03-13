f5db5ce5e8d4c37657f2c6d7a6108e3dce0b7e59
BrixPyxis <danieloche@gmail.com>
2020-03-13 16:22:43 +0100

    MAj task set : utilisation de stdIn

--- tasksBench/scriptGenerationFichiers.sh
+++ tasksBench/scriptGenerationFichiers.sh
@@ -1,10 +1,58 @@
 #! /bin/bash
 
-names=("basicmath_S" "basicmath_L" "bitcount_S" "bitcount_L" "qsort_S" "qsort_L" "susan_S_smooth" "susan_S_edges" "susan_S_corners" "susan_L_smooth" "susan_L_edges" "susan_L_corners" "cjpeg_S" "cjpeg_L" "djpeg_S" "djpeg_L" "typeset_S" "typeset_L" "dijkstra_S" "dijkstra_L" "patricia_S" "patricia_L" "stringsearch_L" "stringsearch_S" "blowfishE_S" "blowfishD_S" "blowfishE_L" "blowfishD_L" "sha_S" "sha_L" "adpcmCaudio_S" "adpcmCaudio_L" "adpcmDaudio_S" "adpcmDaudio_L" "CRC32_S" "CRC32_L" "fft_S" "fft_L" "fft_inv_S" "fft_inv_L" "gsmToast_S" "gsmToast_L" "gsmUToast_S" "gsmUToast_L" "rijndaelE_S" "rijndaelE_L" "rijndaelD_S" "rijndaelD_L")
-
+names=("adpcmCaudio_L"
+"adpcmCaudio_S"
+"adpcmDaudio_L"
+"adpcmDaudio_S"
+"basicmath_L"
+"basicmath_S"
+"bitcount_L"
+"bitcount_S"
+"blowfishD_L"
+"blowfishD_S"
+"blowfishE_L"
+"blowfishE_S"
+"cjpeg_L"
+"cjpeg_S"
+"CRC32_L"
+"CRC32_S"
+"dijkstra_L"
+"dijkstra_S"
+"djpeg_L"
+"djpeg_S"
+"fft_inv_L"
+"fft_inv_S"
+"fft_L"
+"fft_S"
+"gsmToast_L"
+"gsmToast_S"
+"gsmUToast_L"
+"gsmUToast_S"
+"patricia_L"
+"patricia_S"
+"qsort_L"
+"qsort_S"
+"rijndaelD_L"
+"rijndaelD_S"
+"rijndaelE_L"
+"rijndaelE_S"
+"sha_L"
+"sha_S"
+"stringsearch_L"
+"stringsearch_S"
+"susan_L_corners"
+"susan_L_edges"
+"susan_L_smooth"
+"susan_S_corners"
+"susan_S_edges"
+"susan_S_smooth"
+"load_L"
+"load_S"
+"typeset_L"
+"typeset_S")
 
 inputFile=./baseFile_funcs.csv
-DO=func # default behaviour
+DO=func         # default behaviour
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -28,22 +76,25 @@ then
 	do
 	    cp $inputFile ./${n}.tmp
 	    numLigne=0
-	    while read -r firstWord restLine # Premier nom de la ligne
+	    while read -r firstWord garbage restLine # Premier nom de la ligne
 	    do
-		if test $numLigne == 0
-		then
-		    echo "ID  name 		     meanET  wcet    isHRT aff  prec path                       arguments"
+		if test $numLigne == 0 ; then
+		    echo "ID  name 	wcet  period isHRT aff  prec func                       arguments" > ./${n}.in
 		    numLigne=`expr $numLigne + 1`
-		else
-		    if test $n == $firstWord
-		    then
-		        echo $numLigne $firstWord $restLine
+		elif test $n == $firstWord ; then
+		        echo $numLigne $firstWord $restLine		>> ./${n}.in
 		        numLigne=`expr $numLigne + 1`
-		    fi
+		elif test $n == $garbage -a $firstWord == "//" ; then
+			restLine=`echo ${restLine//\"/}`
+			echo $firstWord $restLine			>> ./${n}.in
+		        numLigne=`expr $numLigne + 1`
 		fi
-	    done < ${n}.tmp > ./${n}.in
+	    done < ${n}.tmp
 	    rm ./${n}.tmp
 	done
+
+
+
 else  # DO="exec"
 	for n in ${names[*]}
 	do
